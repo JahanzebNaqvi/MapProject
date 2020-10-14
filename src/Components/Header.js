30c4c328aff1f478ad5d0de58974778ff0d8afae
unknown <j.naqvi74@gmail.com>
2020-10-14 21:31:17 +0500

    added fontawesome icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignOutAlt, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 function Header() {
   let classTab = "activeTab Tab tabShadow";
   return (
     <Row className="header">
       <Col md="6" lg="6">
         <Row>
-          <Col md="4" className="Tab">
-            logout
+          <Col md="1" className="Tab">
+            <FontAwesomeIcon icon={faUserCircle} size="lg" fixedWidth />
+          </Col>
+          <Col md="1" className="Tab">
+            <FontAwesomeIcon
+              icon={faSignOutAlt}
+              size="lg"
+              color="green"
+              fixedWidth
+            />
+          </Col>
+          <Col md="1" className="Tab">
+            Logout
           </Col>
         </Row>
       </Col>
